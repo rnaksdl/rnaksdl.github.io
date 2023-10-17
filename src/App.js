3fc1658d4163a5a09025d97302da99c3791c00de
Kyumin Lee <2km4890@gmail.com>
2023-10-17 15:05:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Kyumin Lee</h1>
+        <p>CS student at OU</p>
       </header>
     </div>
   );
