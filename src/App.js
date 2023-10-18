07e30de1e1d0db6ef8f957b2c66675be52da98c9
Kyumin Lee <2km4890@gmail.com>
2023-10-17 21:55:26 -0500

    sticky header for my name

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-// Import images
 import image1 from './assets/IMG_9675.jpg';
 import image2 from './assets/IMG_9772.jpg';
 
 function App() {
+  const [fontSize, setFontSize] = useState('5em');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const yOffset = window.pageYOffset;
+      const newSize = Math.max(2, 5 - yOffset / 100) + 'em';
+      setFontSize(newSize);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="sticky-header">
+      <header className="sticky-header" style={{ fontSize }}>
         <h1>Kyumin Lee</h1>
       </header>
       <section className="section" id="section1" style={{ backgroundImage: `url(${image1})` }}>
         <div className="content">
-          {/* Empty because name is in sticky header */}
         </div>
       </section>
       <section className="section" id="section2" style={{ backgroundImage: `url(${image2})` }}>
