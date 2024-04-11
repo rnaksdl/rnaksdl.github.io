9eb1fd302d9e0836a39835191b49cd78b3a9e5a9
Kyumin Lee <2km4890@gmail.com>
2024-04-10 21:49:58 -0500

    creating thing with JS

---
+++ JS_Playground/script.js
@@ -0,0 +1,43 @@
+/*
+transaction
+	source
+		“Dad”
+		“Claire”
+	amount
+		+ for income
+		- for spending
+	Category
+		“Needs”
+		“wants”
+*/
+
+
+
+// JavaScript code​​​​​​‌​‌​‌‌‌‌​‌‌‌​‌‌​​‌‌‌‌​​​‌ below
+// Change these values to control whether you see 
+// the expected answer and/or hints.
+const showExpectedResult = false
+const showHints = false
+
+// Setup data
+const navContent = `
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Backpacks</a></li>
+      <li><a href="#">Other things</a></li>
+      <li><a href="#">Contact</a></li>
+`;
+
+
+function createNavMenu(document) {
+      // Your code goes here
+      const mainNav = document.createElement("nav");
+      mainNav.classList.add("main-navigation");
+      const navList = document.createElement("ul");
+      navList.innerHTML = navContent
+      mainNav.append(navList)
+      document.querySelector(".siteheader").append(mainNav);
+}
+
+
+
