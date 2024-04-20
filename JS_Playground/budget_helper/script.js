4e4ac835d5a57a25767d35c45db72be6a80ab38a
Kyumin Lee <2km4890@gmail.com>
2024-04-19 23:35:44 -0500

    ALL WORKING!!!

--- JS_Playground/budget_helper/script.js
+++ JS_Playground/budget_helper/script.js
@@ -109,8 +109,7 @@ function calculateTransactions(transactionsBySources) {
 	  }
   
 	  transactionsBySources[source].expenditure = transactionsBySources[source].expenditure_needs + transactionsBySources[source].expenditure_wants;
-	  transactionsBySources[source].balance = transactionsBySources[source].income - transactionsBySources[source].expenditure;
-  
+	  transactionsBySources[source].balance = transactionsBySources[source].income + transactionsBySources[source].expenditure;
 	  total_income += transactionsBySources[source].income;
 	  total_expenditure += transactionsBySources[source].expenditure;
 	  total_balance += transactionsBySources[source].balance;
@@ -128,14 +127,65 @@ function calculateTransactions(transactionsBySources) {
 
 
 
-function display() {
+  function display() {
 	const input = document.querySelector(".input").value;
-	const parsedInput = parseInput(input)
-	console.log(parsedInput)
-	const result = calculateTransactions(parsedInput)
-	document.querySelector(".output").innerHTML = result;
-	console.log(result)
-}
+	const parsedInput = parseInput(input);
+	console.log(parsedInput);
+	const { transactionsBySources, total_income, total_expenditure, total_balance } = calculateTransactions(parsedInput);
+	
+	let output = `
+	  <h2>Transactions by Source</h2>
+	  <table>
+		<thead>
+		  <tr>
+			<th>Source</th>
+			<th>Income</th>
+			<th>Expenditure (Needs)</th>
+			<th>Expenditure (Wants)</th>
+			<th>Total Expenditure</th>
+			<th>Balance</th>
+		  </tr>
+		</thead>
+		<tbody>
+	`;
+	
+	for (const source in transactionsBySources) {
+	  const { name, income, expenditure_needs, expenditure_wants, expenditure, balance } = transactionsBySources[source];
+	  output += `
+		<tr>
+		  <td>${name}</td>
+		  <td>${income}</td>
+		  <td>${expenditure_needs}</td>
+		  <td>${expenditure_wants}</td>
+		  <td>${expenditure}</td>
+		  <td>${balance}</td>
+		</tr>
+	  `;
+	}
+	
+	output += `
+		</tbody>
+	  </table>
+	  <h2>Totals</h2>
+	  <table>
+		<tr>
+		  <th>Total Income</th>
+		  <td>${total_income}</td>
+		</tr>
+		<tr>
+		  <th>Total Expenditure</th>
+		  <td>${total_expenditure}</td>
+		</tr>
+		<tr>
+		  <th>Total Balance</th>
+		  <td>${total_balance}</td>
+		</tr>
+	  </table>
+	`;
+	
+	document.querySelector(".output").innerHTML = output;
+	console.log({ transactionsBySources, total_income, total_expenditure, total_balance });
+  }
 
 
 
